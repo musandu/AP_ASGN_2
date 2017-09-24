5bdf9250c6d5d1c034472f9caaecc7a9056eef34
musandu <mich007el@gmail.com>
2017-09-24 16:54:38 +0200

    Update IdentifierInterface.java

--- src/main/java/IdentifierInterface.java
+++ src/main/java/IdentifierInterface.java
@@ -9,6 +9,11 @@
  **/
 
 public interface IdentifierInterface {
+	
+		//*************change identifier to recieve input token by token by token************* 	
+	
+	
+	
 	/**	@precondition -
      *  @postcondition - returns the String value that the Identifier stores.
      **/
